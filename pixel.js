a26503504ae918a915005b8cd7bce673812165b8
Jarrett Murray Guyer <guyer.jarrett@gmail.com>
2022-10-11 11:32:49 -0600

    improved save functionality

--- pixel.js
+++ pixel.js
@@ -18,6 +18,8 @@ var select = document.querySelector('#color-picker');
 var circle;
 var saveButton = document.querySelector('#save');
 var loadButton = document.querySelector('#load');
+var title = '';
+const parser = new DOMParser();
 
 function makeSquare() {
   let square = document.createElement('square');
@@ -83,29 +85,27 @@ function alter(e) {
   let alteredColor = e.target.value;
   circle.style.backgroundColor = alteredColor;
   mycolor = alteredColor;
+  myborder = alteredColor;
   newColor();
 }
 
 function saveToLocal() {
-  let fullData = [];
-  fullData.push(height);
-  fullData.push(canvas.innerHTML);
-  localStorage.setItem(prompt('Name your creation!'), JSON.stringify(fullData));
+  title = prompt('Name your creation!', title);
+  localStorage.setItem(title, canvas.innerHTML);
 }
 
 function pullFromLocal() {
   let saves = '';
   let load = '';
   let data = '';
-  for (let x in localStorage) {
-    saves += x + '\n';
+  for (let i = 0; i < localStorage.length; i++) {
+    saves += '\n' + localStorage.key(i);
   }
   data = prompt('Which one?\n' + saves);
-  load = JSON.parse(localStorage.getItem(data));
-  height = load[0];
-  console.log(canvas);
-  console.log(load[1]);
-  canvas.innerHTML = load[1];
+  load = localStorage.getItem(data);
+  canvas.innerHTML = load;
+  canvas.addEventListener('mousedown', clickColor);
+  canvas.addEventListener('mouseover', dragColor);
 }
 
 // Actually Running Code
@@ -137,13 +137,7 @@ for (let i = 0; i < palette.children.length - 1; i++) {
     }
     console.log(getComputedStyle(circle).backgroundColor);
     mycolor = getComputedStyle(circle).backgroundColor;
-    myborder = getComputedStyle(circle).borderColor;
+    myborder = mycolor;
     newColor();
   });
 }
-
-// red.addEventListener('mousedown', function (e) {
-//   console.log(getComputedStyle(e.target).backgroundColor);
-//   mycolor = getComputedStyle(e.target).backgroundColor;
-//   newColor();
-// });
